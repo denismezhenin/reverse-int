f10c2d910cfab2bf529882d1866560b5cc64c230
denismezhenin <dzianismiazhenin@gmail.com>
2022-07-12 18:08:53 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let bac = Math.abs(n).toString();
+    // console.log(typeof bac)
+    for (let i = 0; i < bac.length; i++) {
+   result = `${bac[i]}${result}`;
+  }
+  return (Number(result));
 }
